620ade96e331430b00477c991a653f2c0a5bc97f
alhuo-smile <alexandre.huon@smile.fr>
2019-07-29 16:37:55 +0200

    Added Query params + default

--- src/PortalContracts/Feature/Pagination/WebservicePaginationInterface.php
+++ src/PortalContracts/Feature/Pagination/WebservicePaginationInterface.php
@@ -8,4 +8,9 @@ interface WebservicePaginationInterface
     public const PAGINATION_PAGE_COUNT   = 'x-pagination-page-count';
     public const PAGINATION_CURRENT_PAGE = 'x-pagination-current-page';
     public const PAGINATION_PER_PAGE     = 'x-pagination-per-page';
+
+    public const QUERY_PARAM_CURRENT_PAGE   = 'currentPage';
+    public const QUERY_PARAM_ITEMS_PER_PAGE = 'itemsPerPage';
+
+    public const DEFAULT_ITEMS_PER_PAGE = 20;
 }
